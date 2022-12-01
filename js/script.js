db91355e580e0d82a1dfb43321aa1eefcee2f58f
unknown <maruf0108.1@g,ail.com>
2022-12-01 23:23:15 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let loginForm = document.querySelector('.header .login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');    
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
